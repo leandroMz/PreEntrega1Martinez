1cb126e0da03d40a03ed9671b6216d32f61ed77a
Leandro <lean3dro@gmail.com>
2023-05-10 10:52:11 -0300

    actualizandoCard

--- src/components/CartContainer/CartContainer.jsx
+++ src/components/CartContainer/CartContainer.jsx
@@ -16,7 +16,7 @@ export const CartContainer = () => {
     </div>
   }
   return (
-    <div>
+    <div className='orderItems'>
 
       <h2>Carrito ( {totalQuantity() } )  </h2>
       {console.log()}
@@ -30,18 +30,28 @@ export const CartContainer = () => {
         :
         <div>
           {cartList.map(product => (
-            <div>
-              <img src={product.picture} alt="" />
-              <h3>detalle:{product.detail}</h3>
-              <label>cantidad: {product.quantity} </label>
-              <label>precio: {product.price} </label>
-              <h4>Subtotal: {(parseInt(product.price) * parseInt(product.quantity))} </h4>
-              <button onClick={() => quitarProducto(product.id)}>Eliminar del Carrito</button>
+
+            
+            <div className="card">
+              <img className="card-img-top" src={product.picture} alt="" />
+<div className="card-body">
+              <h5>Producto: {product.name}</h5>
+              <p className="card-text">Detalle: {product.detail}</p>
+              <p className="card-text">cantidad: {product.quantity} </p>
+              <label className="card-text">precio: ${product.price} </label>
+              <h4 className="card-text">Subtotal: ${(parseInt(product.price) * parseInt(product.quantity))} </h4>
+              <button className="card-footer" onClick={() => quitarProducto(product.id)}>Eliminar del Carrito</button>
+</div>
+           
             </div>
           ))}
-          <h2>Total: {totalPrice()} </h2>
+          <div className="card">
+          <h2 className="card-text">Total: ${totalPrice()} </h2>
+          <div className="card-footer">
           <button onClick={vaciarCart} className="btn btn-outline-danger">Vaciar Carrito</button>
           <Link to={'/'} className="btn btn-outline-success">Seguir la compra</Link>
+          </div>
+          </div>
         </div>
       }
     </div>
